c65bedbaccbda935ecedcaa74c799302be0cbfaf
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2021-10-03 14:47:58 -0300

    Base pronta

---
+++ bytebankapi/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.green[900],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
